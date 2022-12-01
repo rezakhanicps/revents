49842e7c7e62b9da6915fa0eccd1f0596b9b782f
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-12-01 10:17:53 +0330

    Refactor and Add Semantic UI

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
