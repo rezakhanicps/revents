fc552e4516a85abd52bfe7830c15f06cc586c159
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-12-03 19:35:55 +0330

    Disable submit button when we have no changes

--- src/features/events/event-form/event-form.tsx
+++ src/features/events/event-form/event-form.tsx
@@ -58,46 +58,55 @@ export const EventForm: React.FC<EventFormProps> = ({ match, history }) => {
                     history.push('/events');
                 }}
             >
-                <Form className='ui form'>
-                    <Header sub color='teal' content='Event Details' />
-                    <MyTextInput name='title' placeholder='Event title' />
-                    <MySelectInput
-                        name='category'
-                        placeholder='Category'
-                        options={categoryData}
-                    />
-                    <MyTextArea
-                        name='description'
-                        placeholder='Description'
-                        rows={3}
-                    />
-                    <Header sub color='teal' content='Event Location Details' />
-                    <MyTextInput name='city' placeholder='City' />
-                    <MyTextInput name='venue' placeholder='Venue' />
-                    <MyDateInput
-                        name='date'
-                        placeholderText='Event date'
-                        onChange={() => null}
-                        timeFormat='HH:mm'
-                        showTimeSelect
-                        timeCaption='time'
-                        dateFormat='MMM d, yyy h:mm a'
-                    />
+                {({ isSubmitting, dirty, isValid }) => (
+                    <Form className='ui form'>
+                        <Header sub color='teal' content='Event Details' />
+                        <MyTextInput name='title' placeholder='Event title' />
+                        <MySelectInput
+                            name='category'
+                            placeholder='Category'
+                            options={categoryData}
+                        />
+                        <MyTextArea
+                            name='description'
+                            placeholder='Description'
+                            rows={3}
+                        />
+                        <Header
+                            sub
+                            color='teal'
+                            content='Event Location Details'
+                        />
+                        <MyTextInput name='city' placeholder='City' />
+                        <MyTextInput name='venue' placeholder='Venue' />
+                        <MyDateInput
+                            name='date'
+                            placeholderText='Event date'
+                            onChange={() => null}
+                            timeFormat='HH:mm'
+                            showTimeSelect
+                            timeCaption='time'
+                            dateFormat='MMM d, yyy h:mm a'
+                        />
 
-                    <Button
-                        type='submit'
-                        floated='right'
-                        positive
-                        content='Submit'
-                    />
-                    <Button
-                        as={Link}
-                        to='/events'
-                        type='submit'
-                        floated='right'
-                        content='Cancel'
-                    />
-                </Form>
+                        <Button
+                            loading={isSubmitting}
+                            disabled={!isValid || !dirty || isSubmitting}
+                            type='submit'
+                            floated='right'
+                            positive
+                            content='Submit'
+                        />
+                        <Button
+                            disabled={isSubmitting}
+                            as={Link}
+                            to='/events'
+                            type='submit'
+                            floated='right'
+                            content='Cancel'
+                        />
+                    </Form>
+                )}
             </Formik>
         </Segment>
     );
